2402d0522819560f236e138f857854dd19509cdd
Matthias Schwarz <70815012+matthiasschwarz@users.noreply.github.com>
2020-09-07 16:37:06 +0200

    Add test cases for private videos

--- test/videoInfo.test.ts
+++ test/videoInfo.test.ts
@@ -10,12 +10,14 @@ describe('getVideoInfo', () => {
     await getVideoInfo('').then(value => {
       expect(value.validId).toBeFalsy()
       expect(value.existing).toBeFalsy()
+      expect(value.private).toBeUndefined()
       expect(value.info).toBeUndefined()
     })
 
     await getVideoInfo('123456789abc').then(value => {
       expect(value.validId).toBeFalsy()
       expect(value.existing).toBeFalsy()
+      expect(value.private).toBeUndefined()
       expect(value.info).toBeUndefined()
     })
   })
@@ -24,6 +26,7 @@ describe('getVideoInfo', () => {
     await getVideoInfo('00000000000').then(value => {
       expect(value.validId).toBeTruthy()
       expect(value.existing).toBeFalsy()
+      expect(value.private).toBeUndefined()
       expect(value.info).toBeUndefined()
     })
   })
@@ -32,6 +35,7 @@ describe('getVideoInfo', () => {
     await getVideoInfo('M7lc1UVf-VE').then(value => {
       expect(value.validId).toBeTruthy()
       expect(value.existing).toBeTruthy()
+      expect(value.private).toBeFalsy()
       expect(value.info).toEqual({
         title: 'YouTube Developers Live: Embedded Web Player Customization',
         author: {
@@ -41,9 +45,18 @@ describe('getVideoInfo', () => {
       })
     })
   })
+
+  it('should resolve as private video', async () => {
+    await getVideoInfo('ZFxT6d13OKo').then(value => {
+      expect(value.validId).toBeTruthy()
+      expect(value.existing).toBeTruthy()
+      expect(value.private).toBeTruthy()
+      expect(value.info).toBeUndefined()
+    })
+  })
 })
 
-describe('getVideoInfo example', () => {
+describe('getVideoInfo example existing check', () => {
   it('should run async', () => {
     getVideoInfo('M7lc1UVf-VE').then(value => {
       if (value.existing) console.log('Video exists!')
@@ -53,5 +66,18 @@ describe('getVideoInfo example', () => {
   it('should run blocked', async () => {
     const existing = (await getVideoInfo('M7lc1UVf-VE')).existing
     if (existing) console.log('Video exists!')
+  })
+})
+
+describe('getVideoInfo example private check', () => {
+  it('should run async', () => {
+    getVideoInfo('ZFxT6d13OKo').then(value => {
+      if (value.existing && value.private) console.log('Video is private!')
+    })
+  })
+
+  it('should run blocked', async () => {
+    const response = await getVideoInfo('ZFxT6d13OKo')
+    if (response.existing && response.private) console.log('Video is private!')
   })
 })
